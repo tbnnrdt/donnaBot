03635498554e750001ef6e295a5cc0ac92a5809f
Thomas Bonnardot <thomas.bonnardot@hei.fr>
2017-08-04 14:05:04 +0200

    commit 4

--- index.js
+++ index.js
@@ -21,12 +21,47 @@ bot.on('message', (payload, reply) => {
   bot.getProfile(payload.sender.id, (err, profile) => {
     if (err) throw err
 
-    reply({ text }, (err) => {
+    console.log(`Echoed back to ${profile.id} - ${profile.first_name} ${profile.last_name}: ${text}`)
+    /*reply({ text }, (err) => {
       if (err) throw err
 
-      console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
-    })
+      console.log(`Echoed back to ${profile.id} - ${profile.first_name} ${profile.last_name}: ${text}`)
+    })*/
   })
 })
+bot.hear(['hello', 'hi', /hey( there)?/i], (payload, chat) => {
+	// Send a text message followed by another text message that contains a typing indicator
+	chat.say('Hello, human friend!').then(() => {
+		chat.say('How are you today?', { typing: true });
+	});
+});
+
+bot.hear(['food', 'hungry'], (payload, chat) => {
+	// Send a text message with quick replies
+	chat.say({
+		text: 'What do you want to eat today?',
+		quickReplies: ['Mexican', 'Italian', 'American', 'Argentine']
+	});
+});
+
+bot.hear(['help'], (payload, chat) => {
+	// Send a text message with buttons
+	chat.say({
+		text: 'What do you need help with?',
+		buttons: [
+			{ type: 'postback', title: 'Settings', payload: 'HELP_SETTINGS' },
+			{ type: 'postback', title: 'FAQ', payload: 'HELP_FAQ' },
+			{ type: 'postback', title: 'Talk to a human', payload: 'HELP_HUMAN' }
+		]
+	});
+});
+
+bot.hear('image', (payload, chat) => {
+	// Send an attachment
+	chat.say({
+		attachment: 'image',
+		url: 'http://example.com/image.png'
+	});
+});
 http.createServer(bot.middleware()).listen(process.env.PORT)
 console.log('Server is running.')
